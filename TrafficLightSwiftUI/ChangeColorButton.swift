ae612c28096c4f29bd8348728fc0525c93ddda38
Teo <teccent@yandex.ru>
2024-02-11 16:08:02 +0300

    изменение opacity через тернарный оператор

---
+++ TrafficLightSwiftUI/ChangeColorButton.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeColorButton.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Теона Магай on 11.02.2024.
+//
+
+import SwiftUI
+
+struct ChangeColorButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeColorButton()
+}
---
+++ TrafficLightSwiftUI/ColorCircle.swift
@@ -0,0 +1,18 @@
+//
+//  ColorCircle.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Теона Магай on 11.02.2024.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorCircle()
+}
